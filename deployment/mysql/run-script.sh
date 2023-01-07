54517542f11f041323cab2c102a1e53bab87e4dd
larycoder <lenhuchuhiep99@gmail.com>
2023-01-07 16:28:11 +0700

    deployment: add phpmyadmin for easier configuration

--- deployment/mysql/run-script.sh
+++ deployment/mysql/run-script.sh
@@ -16,3 +16,13 @@ echo """
 # Prepare MySql Schema following file: mailserver.sql
 docker exec -i smtp_sc-mysql mysql -uroot -proot < ./mailserver.sql
 """
+
+echo """
+# Debugging MySql => we use PHPMyadmin
+docker run -d --rm \
+    --network smtp_sc-network \
+    --name smtp_sc-phpmyadmin \
+    -e PMA_HOST='smtp_sc-mysql' \
+    -p 8080:80 \
+    phpmyadmin/phpmyadmin:latest
+"""
