6427767c8e846545bd7daf1991282ef0afcd9e21
larycoder <lenhuchuhiep99@gmail.com>
2023-01-08 00:23:41 +0700

    deployment: [fix] set default for clean warning question

--- deployment/mysql/clean.sh
+++ deployment/mysql/clean.sh
@@ -8,13 +8,14 @@ This script is not safety. Do you want to continue ? [N/y] """;
 read answer;
 answer=${answer:0:1};
 
-if [[ ${answer^^} == 'N' ]];
+if [[ ${answer^^} == 'N' || $answer == '' ]];
 then
     echo "OK stop here";
     exit 0;
 elif [[ ${answer^^} == 'Y' ]];
 then
     echo "Continue";
+    echo "";
 else
     echo "Could not recognize answer...";
     exit 1;
